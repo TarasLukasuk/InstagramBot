e829f5316adbe44cd4f66eb1d16bc9fb632d911b
TarasLukasuk <121807502+TarasLukasuk@users.noreply.github.com>
2024-02-26 22:26:56 +0200

    "Implement threaded execution and input validation in InstagramBotGUI"
    Description:
    
    In this commit, I introduced a threaded execution mechanism to the InstagramBotGUI, ensuring that the bot's tasks run in a separate thread to maintain a responsive user interface. Additionally, input validation has been implemented to enhance the robustness of user interactions. This feature ensures that essential fields, such as username and password, are validated before initiating bot actions, providing a more user-friendly experience and preventing potential errors.
    
    These improvements contribute to a smoother and more reliable user experience when utilizing the Instagram Bot through the graphical interface.

--- instagramBot/main.py
+++ instagramBot/main.py
@@ -1,6 +1,7 @@
 from ui import InstagramBotGUI
 import tkinter as tk
 
+
 def main():
     root = tk.Tk()
     root.iconbitmap('create_an_icon_that_shows_sending_messages_to_Instagram_S1278494694_St25_G7.5.ico')
